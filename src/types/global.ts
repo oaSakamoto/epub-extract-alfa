0f40f6af649a79c6609d574cd954a788bcb569c2
Olimpio Almeida Sakamoto <oasakamoto@proton.me>
2025-02-18 00:31:48 -0300

    feat: Add global types for EpubConfig and Resources
    Introduces new types `EpubConfig`, `ResourcesMap`, and
    `ResourcesCategories` in `src/types/global.ts` to define the structure
    for storing EPUB configuration and resources.  This improves type safety
    and code clarity when working with EPUB data.

---
+++ src/types/global.ts
@@ -0,0 +1,14 @@
+import { EPUB_DEFAULTS } from "@/unpack/config";
+
+export type EpubConfig = typeof EPUB_DEFAULTS;
+export type ResourcesMap = Map<string, Blob>;
+
+export type ResourcesCategories = {
+  css: ResourcesMap;
+  html: ResourcesMap;
+  images: ResourcesMap;
+  xml: ResourcesMap;
+  xhtml: ResourcesMap;
+  opf: ResourcesMap;
+  ncx: ResourcesMap;
+};
